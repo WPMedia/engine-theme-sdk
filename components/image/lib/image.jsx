40389fa39399ec3721513c8c7bab3cca811a59b7
Brent Miller <brent.miller@washport.com>
2019-08-19 15:17:52 -0500

    initial stab at this

---
+++ components/image/lib/image.jsx
@@ -0,0 +1,52 @@
+/* eslint react/destructuring-assignment: "off", no-param-reassign: "off" */
+import React, { Component } from 'react';
+import Static from 'fusion:static';
+import buildThumborURL from './thumbor-image-url';
+
+
+class Image extends Component {
+    static DEFAULT_IMAGE = 'resources/images/default_feed_image.jpg';
+
+    constructor(props) {
+      super(props);
+      this.defaultImage = this.props.defaultImage || Image.DEFAULT_IMAGE;
+    }
+
+    addDefaultSrc(elm) {
+      elm.target.src = `${Fusion.contextPath}/${this.defaultImage}`;
+    }
+
+    render() {
+      const {
+        url,
+        alt,
+        smallWidth,
+        smallHeight,
+        mediumWidth,
+        mediumHeight,
+        largeWidth,
+        largeHeight,
+      } = this.props;
+
+      return (
+        <Static id={url}>
+          <img
+            className="lazy"
+            onError={this.addDefaultSrc}
+            src={this.addDefaultSrc}
+            data-src={buildThumborURL(url, smallWidth,
+              smallHeight)}
+            data-srcset={`
+          ${buildThumborURL(url, mediumWidth,
+              mediumHeight)} 1000w, 
+          ${buildThumborURL(url, largeWidth,
+                largeHeight)} 2000w
+          `}
+            alt={alt}
+          />
+        </Static>
+      );
+    }
+}
+
+export default Image;
