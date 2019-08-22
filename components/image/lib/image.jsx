ef4e278bb7fe64c9b258424b2c73e6972a1f11b0
Brent Miller <brent.miller@washport.com>
2019-08-22 16:42:22 -0500

    Updated default image path

--- components/image/lib/image.jsx
+++ components/image/lib/image.jsx
@@ -1,25 +1,30 @@
 /* eslint react/destructuring-assignment: "off", no-param-reassign: "off" */
 import React, { Component } from 'react';
 import Static from 'fusion:static';
-import buildThumborURL from './thumbor-image-url';
+import buildThmborURL from './thumbor-image-url';
 
 /**
  * Image component that has basic Thumbor and lazy loading support.
  *
  * The Thumbor support assumes that the feature pack leveraging this component will have the
- * packaage "thumbor-lite" installed via NPM. The requirement for thumbor-lite is through a dynamic require to enforce
- * its usaage server-side...See the file in this repository: components/image/lib/thumbor-image-url.js
+ * packaage "thumbor-lite" installed via NPM. The requirement for thumbor-lite is through a
+ * dynamic require to enforce
+ * its usaage server-side...See the file in this repository:
+ * components/image/lib/thumbor-image-url.js
  *
- * Also, the lazy loading assumes that that the feature pack will have the Yall library (https://github.com/malchata/yall.js)
- * installed in resources/js/yall.min.js along with an additional script to bootstrap the library.  For example, a script called
- * resources/js/image-lazy.js and basically has this one line of code:  document.addEventListener('DOMContentLoaded', yall).
+ * Also, the lazy loading assumes that that the feature pack will have the
+ * Yall library (https://github.com/malchata/yall.js)
+ * installed in resources/js/yall.min.js along with an additional script to bootstrap the library.
+ * For example, a script called
+ * resources/js/image-lazy.js and basically has this one line of code:
+ * document.addEventListener('DOMContentLoaded', yall).
  * These two scripts are then included in an output-type file like this:
  *
  * <script type="text/javascript" src={deployment(`${contextPath}/resources/js/yall.min.js`)} />
  * <script type="text/javascript" src={deployment(`${contextPath}/resources/js/image-lazy.js`)} />
  *
- * Note: This component needs to be extended to allow for Thumbor ratio and focal point values that are set by the
- * producer.
+ * Note: This component needs to be extended to allow for Thumbor ratio and focal point values
+ * that are set by the producer.
  *
  * @constructor
  * @param {string} URL - URL to the unoptimized image.
@@ -32,12 +37,13 @@ import buildThumborURL from './thumbor-image-url';
  * @param {number} largeHeight - Height of the image to crop to for the large break point
  */
 class Image extends Component {
-    static getDefaultImagePath() {
-        return '/pf/resources/images/default_feed_image.jpg';
+    getDefaultImagePath() {
+        const { deployment } = this.props;
+        return deployment('/pf/resources/images/default_feed_image.jpg');
     }
 
-    static addDefaultSrc(elm) {
-        elm.target.src = Image.getDefaultImagePath(); // eslint-disable-line no-param-reassign
+    addDefaultSrc(elm) {
+        elm.target.src = this.getDefaultImagePath(); // eslint-disable-line no-param-reassign
     }
 
     render() {
@@ -57,7 +63,7 @@ class Image extends Component {
          * the default image, it won't be able to get sent over to Thumbor for
          * processing and it will fail.
          */
-        if (process.env.NODE_ENV === 'development') {
+        if (url.indexOf('/pf/') !== -1) {
             return (
                 <Static id={url}>
                     <img
@@ -72,14 +78,14 @@ class Image extends Component {
             <Static id={url}>
                 <img
                     className="lazy"
-                    onError={Image.addDefaultSrc}
-                    src={Image.addDefaultSrc}
-                    data-src={buildThumborURL(url, smallWidth,
+                    onError={this.addDefaultSrc}
+                    src={this.addDefaultSrc}
+                    data-src={buildThmborURL(url, smallWidth,
                         smallHeight)}
                     data-srcset={`
-          ${buildThumborURL(url, mediumWidth,
+          ${buildThmborURL(url, mediumWidth,
                         mediumHeight)} 1000w, 
-          ${buildThumborURL(url, largeWidth,
+          ${buildThmborURL(url, largeWidth,
                         largeHeight)} 2000w
           `}
                     alt={alt}
