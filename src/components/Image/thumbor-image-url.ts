4a7100eaaee57c56a3398f2a0d2364ecdd706eac
Joe Grosspietsch <joe.grosspietsch@washpost.com>
2019-12-10 11:37:24 -0600

    Fixing capitalization

---
+++ src/components/Image/thumbor-image-url.ts
@@ -0,0 +1,18 @@
+import { resizerURL, resizerKey } from 'fusion:environment';
+
+const buildThumborURL = (url: string, displayWidth: number, displayHeight: number): string => {
+  if (typeof window === 'undefined') {
+    const Thumbor = require('thumbor-lite'); // eslint-disable-line global-require,@typescript-eslint/no-var-requires
+    const thumbor = new Thumbor(resizerKey, resizerURL);
+    const imgSrc = url.replace(/^http[s]?:\/\//, '')
+      .replace(' ', '%20');
+    if (imgSrc.includes('?')) imgSrc.replace('?', '%3F');
+
+    return thumbor.setImagePath(imgSrc)
+      .resize(displayWidth, displayHeight)
+      .buildUrl();
+  }
+  return '';
+};
+
+export default buildThumborURL;
