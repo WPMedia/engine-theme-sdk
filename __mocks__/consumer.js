8bced18a58a2955b8ce83ee78873cf3ef9141118
Joe Grosspietsch <joe.grosspietsch@washpost.com>
2019-12-16 21:01:46 -0600

    Should delete these extra mocks

--- __mocks__/consumer.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable @typescript-eslint/explicit-function-return-type */
-function Consumer() {
-  return null;
-}
-
-export default Consumer;
--- __mocks__/content.js
+++
@@ -1,3 +0,0 @@
-export default {
-  useContent: jest.fn(),
-};
