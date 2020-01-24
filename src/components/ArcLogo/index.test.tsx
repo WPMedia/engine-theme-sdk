00de53d4197ecec335c401981d2283e462778e0e
Joe Grosspietsch <joe.grosspietsch@washpost.com>
2020-01-24 11:39:37 -0600

    Added ArcLogo component

---
+++ src/components/ArcLogo/index.test.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import ArcLogo from '.';
+
+describe('the ArcLogo component', () => {
+  it('should render an SVG', () => {
+    const wrapper = shallow(<ArcLogo />);
+    expect(wrapper.name()).toEqual('svg');
+  });
+
+  describe('the title prop', () => {
+    it('should set the title of the svg element', () => {
+      const wrapper = shallow(<ArcLogo title="an icon!" />);
+      expect(wrapper.find('title').text()).toEqual('an icon!');
+    });
+
+    it('should default to an empty string', () => {
+      const wrapper = shallow(<ArcLogo />);
+      expect(wrapper.find('title').text()).toEqual('');
+    });
+  });
+
+  describe('the description prop', () => {
+    it('should set the description of the svg element', () => {
+      const wrapper = shallow(<ArcLogo description="this is an icon!" />);
+      expect(wrapper.find('desc').text()).toEqual('this is an icon!');
+    });
+
+    it('should default to an empty string', () => {
+      const wrapper = shallow(<ArcLogo />);
+      expect(wrapper.find('desc').text()).toEqual('');
+    });
+  });
+});
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
+export { default as ArcLogo } from './ArcLogo';
 export { default as Gallery } from './Gallery';
 export { default as Image } from './Image';
 export { default as ImageMetadata } from './ImageMetadata';
