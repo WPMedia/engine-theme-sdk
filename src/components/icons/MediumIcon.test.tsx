713413ed1ebeac371d9013229fcade641b7d68d8
Joe Grosspietsch <joe.grosspietsch@washpost.com>
2020-02-11 15:26:06 -0600

    Added icons for Medium, Reddit, Snapchat, Tumblr, and WhatsApp

---
+++ src/components/icons/MediumIcon.test.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import MediumIcon from './MediumIcon';
+
+describe('the MediumIcon component', () => {
+  it('should render an SVG', () => {
+    const wrapper = shallow(<MediumIcon />);
+    expect(wrapper.name()).toEqual('svg');
+  });
+
+  describe('the width prop', () => {
+    it('should set the width of the svg element', () => {
+      const wrapper = shallow(<MediumIcon width={42} />);
+      expect(wrapper.prop('width')).toEqual(42);
+    });
+
+    it('should default to 24', () => {
+      const wrapper = shallow(<MediumIcon />);
+      expect(wrapper.prop('width')).toEqual(24);
+    });
+  });
+
+  describe('the height prop', () => {
+    it('should set the height of the svg element', () => {
+      const wrapper = shallow(<MediumIcon height={42} />);
+      expect(wrapper.prop('height')).toEqual(42);
+    });
+
+    it('should default to 24', () => {
+      const wrapper = shallow(<MediumIcon />);
+      expect(wrapper.prop('height')).toEqual(24);
+    });
+  });
+
+  describe('the fill prop', () => {
+    it('should set the fill color of the svg element', () => {
+      const wrapper = shallow(<MediumIcon fill="#222222" />);
+      expect(wrapper.find('path').prop('fill')).toEqual('#222222');
+    });
+
+    it('should default to black', () => {
+      const wrapper = shallow(<MediumIcon />);
+      expect(wrapper.find('path').prop('fill')).toEqual('#000');
+    });
+  });
+
+  describe('the title prop', () => {
+    it('should set the title of the svg element', () => {
+      const wrapper = shallow(<MediumIcon title="an icon!" />);
+      expect(wrapper.find('title').text()).toEqual('an icon!');
+    });
+
+    it('should default to an empty string', () => {
+      const wrapper = shallow(<MediumIcon />);
+      expect(wrapper.find('title').text()).toEqual('Medium logo');
+    });
+  });
+
+  describe('the description prop', () => {
+    it('should set the description of the svg element', () => {
+      const wrapper = shallow(<MediumIcon description="this is an icon!" />);
+      expect(wrapper.find('desc').text()).toEqual('this is an icon!');
+    });
+
+    it('should default to an empty string', () => {
+      const wrapper = shallow(<MediumIcon />);
+      expect(wrapper.find('desc').text()).toEqual('');
+    });
+  });
+});
---
+++ src/components/icons/RedditIcon.test.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import RedditIcon from './RedditIcon';
+
+describe('the RedditIcon component', () => {
+  it('should render an SVG', () => {
+    const wrapper = shallow(<RedditIcon />);
+    expect(wrapper.name()).toEqual('svg');
+  });
+
+  describe('the width prop', () => {
+    it('should set the width of the svg element', () => {
+      const wrapper = shallow(<RedditIcon width={42} />);
+      expect(wrapper.prop('width')).toEqual(42);
+    });
+
+    it('should default to 24', () => {
+      const wrapper = shallow(<RedditIcon />);
+      expect(wrapper.prop('width')).toEqual(24);
+    });
+  });
+
+  describe('the height prop', () => {
+    it('should set the height of the svg element', () => {
+      const wrapper = shallow(<RedditIcon height={42} />);
+      expect(wrapper.prop('height')).toEqual(42);
+    });
+
+    it('should default to 24', () => {
+      const wrapper = shallow(<RedditIcon />);
+      expect(wrapper.prop('height')).toEqual(24);
+    });
+  });
+
+  describe('the fill prop', () => {
+    it('should set the fill color of the svg element', () => {
+      const wrapper = shallow(<RedditIcon fill="#222222" />);
+      expect(wrapper.find('path').prop('fill')).toEqual('#222222');
+    });
+
+    it('should default to black', () => {
+      const wrapper = shallow(<RedditIcon />);
+      expect(wrapper.find('path').prop('fill')).toEqual('#000');
+    });
+  });
+
+  describe('the title prop', () => {
+    it('should set the title of the svg element', () => {
+      const wrapper = shallow(<RedditIcon title="an icon!" />);
+      expect(wrapper.find('title').text()).toEqual('an icon!');
+    });
+
+    it('should default to an empty string', () => {
+      const wrapper = shallow(<RedditIcon />);
+      expect(wrapper.find('title').text()).toEqual('Reddit logo');
+    });
+  });
+
+  describe('the description prop', () => {
+    it('should set the description of the svg element', () => {
+      const wrapper = shallow(<RedditIcon description="this is an icon!" />);
+      expect(wrapper.find('desc').text()).toEqual('this is an icon!');
+    });
+
+    it('should default to an empty string', () => {
+      const wrapper = shallow(<RedditIcon />);
+      expect(wrapper.find('desc').text()).toEqual('');
+    });
+  });
+});
