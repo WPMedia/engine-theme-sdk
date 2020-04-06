5fa4e71843fec0b3bf084bba3445b35b51914777
jackhowa <jack.howard@washpost.com>
2020-04-06 12:15:46 -0500

    Update linting for storybook

--- stories/Image.stories.tsx
+++ stories/Image.stories.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
-// eslint-disable-next-line import/no-extraneous-dependencies
 import { storiesOf, addParameters } from '@storybook/react';
-// eslint-disable-next-line import/no-extraneous-dependencies
 import {
   INITIAL_VIEWPORTS,
 } from '@storybook/addon-viewport';
