8b39a415ee1bc82ab7f1d4606f3ecbd3bc217d34
jackhowa <jack.howard@washpost.com>
2020-04-06 11:16:51 -0500

    Add image storybook for taking in resizer url

---
+++ stories/Image.stories.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { storiesOf, addParameters } from '@storybook/react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import {
+  INITIAL_VIEWPORTS,
+} from '@storybook/addon-viewport';
+import Image from '../src/components/Image';
+
+addParameters({
+  viewport: {
+    viewports: {
+      ...INITIAL_VIEWPORTS,
+      MacbookPro: {
+        name: 'Macbook pro 1',
+        styles: {
+          width: '1200px',
+          height: '801px',
+        },
+      },
+    },
+  },
+});
+
+const breakpoints = {
+  small: 420,
+  medium: 768,
+  large: 992,
+};
+
+const resizedParams = {
+  '158x89': '/r4YXPy4Eh2thx80bDTxRZM9Syhw=filters:format(jpg):quality(70)/',
+  '274x154': '/sDwhmVtwayjjDJww8CvlWjpydGM=filters:format(jpg):quality(70)/',
+};
+
+const smallWidth = 158;
+const smallHeight = 89;
+const mediumWidth = 274;
+const mediumHeight = 154;
+const largeWidth = 274;
+const largeHeight = 154;
+
+const resizerURL = 'https://corecomponents-the-prophet-prod.cdn.arcpublishing.com/resizer';
+
+const rawURL = 'https://arc-anglerfish-arc2-prod-corecomponents.s3.amazonaws.com/public/37UMUNYNOVCEJDZW5SBKBXNMO4.jpg';
+const alt = 'This is a Free article for testing Mentor Medier’s paywall';
+
+storiesOf('Image', module)
+  .add('with widths and heights', () => (
+    <Image
+      url={rawURL}
+      alt={alt}
+      smallWidth={smallWidth}
+      smallHeight={smallHeight}
+      mediumWidth={mediumWidth}
+      mediumHeight={mediumHeight}
+      largeWidth={largeWidth}
+      largeHeight={largeHeight}
+      resizedImageOptions={resizedParams}
+      resizerURL={resizerURL}
+      breakpoints={breakpoints}
+    />
+  ));
