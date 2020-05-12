60ed95120e0804425fe9f1442b5496ce1d1b0457
jackhowa <jack.howard@washpost.com>
2020-05-12 13:04:46 -0500

    Make sure fitin functionality works too

--- src/components/Image/thumbor-image-url.ts
+++ src/components/Image/thumbor-image-url.ts
@@ -9,6 +9,11 @@ const buildThumborURL = (
   }
   const [targetImageKey, imageFilter = ''] = targetImageKeyWithFilter.split('=');
 
+  if (targetImageKeyWithFilter.includes('fit-in')) {
+    // https://resizer.com/resizer/OzPtTYdwjA0zqIliyUtp5iuF2Hc=//fit-in/377x283/filters:fill(white):background_color(white)/arc-anglerfish-staging-staging.s3.amazonaws.com/public/NA6FMAXWP5DR3FDZQ7SGJ3C3FE.png
+    return `${resizerURL}${targetImageKey}=${imageFilter}${imageSourceWithoutProtocol}`;
+  }
+
   return `${resizerURL}${targetImageKey}=/${targetDimension}/${imageFilter}${imageSourceWithoutProtocol}`;
 };
 
