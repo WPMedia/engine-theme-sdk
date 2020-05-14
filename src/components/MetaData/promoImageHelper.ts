c2e43297c269ecb004bb460449fef3dac019c3ef
Brent Miller <brent.miller@washpost.com>
2020-05-14 13:08:21 -0500

    second attempt at pen-827

---
+++ src/components/MetaData/promoImageHelper.ts
@@ -0,0 +1,49 @@
+import { resizerURL, resizerKey } from 'fusion:environment';
+
+export const getImgURL = (metaValue, metaType = 'og:image', globalContent): string => {
+  const buildURL = (_url): string | null => {
+    if (typeof window === 'undefined') {
+      // eslint-disable-next-line @typescript-eslint/no-var-requires,global-require
+      const Thumbor = require('thumbor-lite');
+      const thumbor = new Thumbor(resizerKey, resizerURL);
+      let imgSrc = _url.replace(/^http[s]?:\/\//, '')
+        .replace(' ', '%20');
+      if (imgSrc.includes('?')) {
+        imgSrc = imgSrc.replace('?', '%3F');
+      }
+
+      return thumbor.setImagePath(imgSrc)
+        .resize(1200, 630)
+        .buildUrl();
+    }
+    return null;
+  };
+
+  if (metaValue(metaType)) {
+    return buildURL(metaValue(metaType));
+  }
+
+  if (globalContent && globalContent.promo_items
+    && globalContent.promo_items.basic
+    && globalContent.promo_items.basic.url) {
+    return buildURL(globalContent.promo_items.basic.url);
+  }
+
+  return '';
+};
+
+export const getImgAlt = (metaValue, metaType = 'og:image:alt', globalContent): string | null => {
+  if (metaValue(metaType)) {
+    return metaValue(metaType);
+  }
+
+  if (globalContent && globalContent.promo_items
+    && globalContent.promo_items.basic
+    && globalContent.promo_items.basic.alt_text) {
+    if (globalContent.promo_items.basic.alt_text) {
+      return globalContent.promo_items.basic.alt_text;
+    }
+  }
+
+  return null;
+};
