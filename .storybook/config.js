0efb181349f86c3c80f580875fdf2ccaee1d53b0
jackhowa <jack.howard@washpost.com>
2020-06-04 16:01:45 -0500

    Add a11y for ensuring components accessible choices

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,8 +1,12 @@
 import { configure } from "@storybook/react"
+import { withA11y } from '@storybook/addon-a11y';
+import { addDecorator } from '@storybook/react';
 
 const req = require.context("../stories", true, /.stories.tsx$/)
 function loadStories() {
   req.keys().forEach(req)
 }
+
+addDecorator(withA11y);
 
 configure(loadStories, module)
