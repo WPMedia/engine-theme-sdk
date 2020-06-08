377acb6f9ae541ef3efc825de887e4e79c2ac9f5
jackhowa <jack.howard@washpost.com>
2020-06-08 14:29:25 -0500

    Remove previous storybook implementation

--- .storybook/main.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  stories: ['../stories/**/*.stories.tsx'],
-  addons: ['@storybook/addon-actions', '@storybook/addon-links', "@storybook/addon-viewport/register", "@storybook/addon-a11y/register"],
-};
