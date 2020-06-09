e9a9f4ed85e2d898127a0246bf039df9d4b38269
jackhowa <jack.howard@washpost.com>
2020-06-09 10:34:24 -0500

    Remove viewport add-on, buggy

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,4 @@
 module.exports = {
   stories: ['../stories/*.stories.jsx'],
-  addons: ['@storybook/addon-actions', '@storybook/addon-links', '@storybook/addon-viewport/register', '@storybook/addon-a11y/register'],
+  addons: ['@storybook/addon-actions', '@storybook/addon-links', '@storybook/addon-a11y/register'],
 };
