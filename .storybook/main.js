446f9f208d75cbdb84a4242f18976cdfacb6724f
beltrancaliz <beltran.caliz@washpost.com>
2020-06-18 16:49:11 -0500

    Event emitter logging story

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,11 @@
 module.exports = {
   stories: ['../stories/*.stories.@(js|jsx|mdx|tsx)'],
-  addons: ['@storybook/addon-a11y/register', '@storybook/addon-backgrounds', '@storybook/addon-docs'],
+  addons: [
+    '@storybook/addon-a11y/register',
+    '@storybook/addon-backgrounds',
+    '@storybook/addon-docs',
+    '@storybook/addon-actions/register'
+  ],
   typescript: {
     // also valid 'react-docgen-typescript' | false
     reactDocgen: 'react-docgen',
