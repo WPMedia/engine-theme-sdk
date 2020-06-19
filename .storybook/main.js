b51a682192c3d9037086c34e4ed31cf4c6567b14
beltrancaliz <beltran.caliz@washpost.com>
2020-06-19 09:23:09 -0500

    Added console logging and knobs addons

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,7 +4,8 @@ module.exports = {
     '@storybook/addon-a11y/register',
     '@storybook/addon-backgrounds',
     '@storybook/addon-docs',
-    '@storybook/addon-actions/register'
+    '@storybook/addon-actions/register',
+    '@storybook/addon-knobs'
   ],
   typescript: {
     // also valid 'react-docgen-typescript' | false
