9d3be2f901907670671c3a472d7173b85c074540
Cheng-Hsin Weng <cheng-hsin.weng@washpost.com>
2020-09-21 15:22:36 -0500

    SENG-63 added video to theme SDK

---
+++ src/components/Video/index.test.jsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import Video from '.';
+
+describe('Video', () => {
+  beforeEach(() => {
+    global.powaBoot = jest.fn()
+  })
+
+  describe('with minimum configurations', () => {
+    it('set proper data attributes and call window.powaBoot', () => {
+      const wrapper = mount(<Video uuid="video-uuid" org="corecomponents" env="prod" />);
+      const videoElement = wrapper.find('.powa').at(0);
+      expect(videoElement.prop('id')).toBe('powa-video-uuid');
+      expect(videoElement.prop('data-org')).toBe('corecomponents');
+      expect(videoElement.prop('data-env')).toBe('prod');
+      expect(videoElement.prop('data-uuid')).toBe('video-uuid');
+      expect(videoElement.prop('data-autoplay')).toBe(false);
+      expect(videoElement.prop('data-playthrough')).toBe(false);
+      expect(videoElement.prop('data-muted')).toBe(false);
+      expect(videoElement.prop('data-aspect-ratio')).toBe(0.5625);
+      expect(global.powaBoot).toBeCalled()
+    });
+  });
+
+  describe('with autoplay', () => {
+    it('set autoplay data attribute to true and call window.powaBoot', () => {
+      const wrapper = mount(<Video uuid="video-uuid" org="corecomponents" env="prod" autoplay />);
+      const videoElement = wrapper.find('.powa').at(0);
+      expect(videoElement.prop('id')).toBe('powa-video-uuid');
+      expect(videoElement.prop('data-org')).toBe('corecomponents');
+      expect(videoElement.prop('data-env')).toBe('prod');
+      expect(videoElement.prop('data-uuid')).toBe('video-uuid');
+      expect(videoElement.prop('data-autoplay')).toBe(true);
+      expect(videoElement.prop('data-playthrough')).toBe(false);
+      expect(videoElement.prop('data-muted')).toBe(true);
+      expect(videoElement.prop('data-aspect-ratio')).toBe(0.5625);
+      expect(global.powaBoot).toBeCalled()
+    });
+  });
+
+  describe('with playthrough', () => {
+    it('set playthrough data attribute to true and call window.powaBoot', () => {
+      const wrapper = mount(<Video uuid="video-uuid" org="corecomponents" env="prod" playthrough />);
+      const videoElement = wrapper.find('.powa').at(0);
+      expect(videoElement.prop('id')).toBe('powa-video-uuid');
+      expect(videoElement.prop('data-org')).toBe('corecomponents');
+      expect(videoElement.prop('data-env')).toBe('prod');
+      expect(videoElement.prop('data-uuid')).toBe('video-uuid');
+      expect(videoElement.prop('data-autoplay')).toBe(false);
+      expect(videoElement.prop('data-playthrough')).toBe(true);
+      expect(videoElement.prop('data-muted')).toBe(false);
+      expect(videoElement.prop('data-aspect-ratio')).toBe(0.5625);
+      expect(global.powaBoot).toBeCalled()
+    });
+  });
+
+  describe('with aspectRatio defined', () => {
+    it('set aspectRatio data attribute to 0.75 and call window.powaBoot', () => {
+      const wrapper = mount(<Video uuid="video-uuid" org="corecomponents" env="prod" aspectRatio={0.75} />);
+      const videoElement = wrapper.find('.powa').at(0);
+      expect(videoElement.prop('id')).toBe('powa-video-uuid');
+      expect(videoElement.prop('data-org')).toBe('corecomponents');
+      expect(videoElement.prop('data-env')).toBe('prod');
+      expect(videoElement.prop('data-uuid')).toBe('video-uuid');
+      expect(videoElement.prop('data-autoplay')).toBe(false);
+      expect(videoElement.prop('data-playthrough')).toBe(false);
+      expect(videoElement.prop('data-muted')).toBe(false);
+      expect(videoElement.prop('data-aspect-ratio')).toBe(0.75);
+      expect(global.powaBoot).toBeCalled()
+    });
+  });
+});
---
+++ src/components/Video/index.tsx
@@ -0,0 +1,67 @@
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+
+declare global {
+  interface Window { powaBoot?: any }
+}
+
+interface VideoProps {
+  uuid: string;
+  org: string;
+  env: string;
+  autoplay?: boolean;
+  playthrough?: boolean;
+  aspectRatio?: number;
+}
+
+const Video: React.FC<VideoProps> = (props) => {
+  const {
+    uuid,
+    org,
+    env,
+    autoplay = false,
+    playthrough = false,
+    // default 16:9
+    aspectRatio = 0.5625,
+  } = props;
+  const muted = autoplay;
+
+  useEffect(() => {
+    if (window.powaBoot) {
+      window.powaBoot();
+    }
+  }, []);
+
+
+  return (
+    <div className="video-container">
+      <div
+        className="powa"
+        id={`powa-${uuid}`}
+        data-org={org}
+        data-env={env}
+        data-uuid={uuid}
+        data-autoplay={autoplay}
+        data-playthrough={playthrough}
+        data-muted={muted}
+        data-aspect-ratio={aspectRatio} />
+    </div>
+  );
+};
+
+Video.propTypes = {
+  /** Video UUID */
+  uuid: PropTypes.string,
+  /** Video org */
+  org: PropTypes.string,
+  /** Video code env */
+  env: PropTypes.string,
+  /** Autoplay the video (video will be muted) */
+  autoplay: PropTypes.bool,
+  /** Play through recommended videos if Video center is configured to do so */
+  playthrough: PropTypes.bool,
+  /** Video ratio */
+  aspectRatio: PropTypes.number,
+};
+
+export default Video;
