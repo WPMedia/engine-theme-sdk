366aa23453e9f2771ee3c5055d23ae52a1f889e8
Brent Miller <brent.miller@washpost.com>
2020-10-01 10:58:36 -0500

    added compression unit test

--- src/components/Image/thumbor-image-url.test.ts
+++ src/components/Image/thumbor-image-url.test.ts
@@ -2,6 +2,7 @@ import buildThumborURL from './thumbor-image-url';
 
 describe('thumbor image url function', () => {
   const targetImageKey = '/r4YXPy4Eh2thx80bDTxRZM9Syhw=filters:format(jpg):quality(70)/';
+  const targetImageKeyCompressed = 'r4YXPy4Eh2thx80bDTxRZM9Syhw=filters:cm=t/';
   const targetImageKeyNoFilters = '/r4YXPy4Eh2thx80bDTxRZM9Syhw=/';
   const imageSource = 'www.hey.com/ffdfdf';
   const resizerURL = 'www.hey.resizer.com';
@@ -13,6 +14,9 @@ describe('thumbor image url function', () => {
   });
   it('returns a well-formed url image if all params present as expected', () => {
     expect(buildThumborURL(targetImageKey, '100x100', imageSource, resizerURL)).toBe('www.hey.resizer.com/r4YXPy4Eh2thx80bDTxRZM9Syhw=/100x100/filters:format(jpg):quality(70)/www.hey.com/ffdfdf');
+  });
+  it('returns a well-formed url image if filters have been assumed defaulted (with the cm=t param) to a format of jpg and quality of 70', () => {
+    expect(buildThumborURL(targetImageKeyCompressed, '100x100', imageSource, resizerURL)).toBe('www.hey.resizer.com/r4YXPy4Eh2thx80bDTxRZM9Syhw=/100x100/filters:format(jpg):quality(70)/www.hey.com/ffdfdf');
   });
   it('retuns well-formed url with no filters', () => {
     expect(buildThumborURL(targetImageKeyNoFilters, '100x100', imageSource, resizerURL)).toBe('www.hey.resizer.com/r4YXPy4Eh2thx80bDTxRZM9Syhw=/100x100//www.hey.com/ffdfdf');
