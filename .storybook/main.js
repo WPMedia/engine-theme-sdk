69536ec5cb99d1877d17359cad4c3bf9a67ee80b
jackhowa <jack.howard@washpost.com>
2020-11-02 17:47:47 -0600

    Upgrade essentials addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,14 +1,8 @@
 module.exports = {
   stories: ['../stories/*.stories.@(js|jsx|mdx|tsx)'],
   addons: [
-    '@storybook/addon-a11y/register',
-    '@storybook/addon-backgrounds',
-    '@storybook/addon-docs',
-    '@storybook/addon-actions/register',
+    '@storybook/addon-a11y',
+    '@storybook/addon-essentials',
     '@storybook/addon-knobs'
-  ],
-  typescript: {
-    // also valid 'react-docgen-typescript' | false
-    reactDocgen: 'react-docgen',
-  },
+  ]
 };
