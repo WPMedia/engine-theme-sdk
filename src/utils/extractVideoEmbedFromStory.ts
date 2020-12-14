12c6a2d214836aa87056dae8d9c385854c2b522f
Vinnie Olsen <vinnie.olsen@gmail.com>
2020-12-14 11:35:32 -0600

    Refactored 'extractVideoEmbedFromStory', added interface

--- src/utils/extractVideoEmbedFromStory.ts
+++ src/utils/extractVideoEmbedFromStory.ts
@@ -1,16 +1,30 @@
-const extractVideoEmbedFromStory = (content) => {
-  if (!content || !content.type) return undefined;
-  const contentEl = (
-    (content.type === 'video' && content) || (
-      content.type === 'story'
-      && content.promo_items
+interface SchemaANS {
+  type: string;
+  promo_items?: {
+    lead_art?: {
+      type?: string;
+      embed_html?: string;
+    };
+  };
+  embed_html?: string;
+}
+
+const extractVideoEmbedFromStory = (content: SchemaANS): string => {
+  if (content && content.type === 'video') {
+    return content.embed_html;
+  }
+
+  if (
+    (content && content.type === 'story') && (
+      content.promo_items
       && content.promo_items.lead_art
-      && content.promo_items.lead_art.type
       && content.promo_items.lead_art.type === 'video'
-        ? content.promo_items.lead_art : undefined
     )
-  );
-  return contentEl ? contentEl.embed_html : undefined;
+  ) {
+    return content.promo_items.lead_art.embed_html;
+  }
+
+  return undefined;
 };
 
 export default extractVideoEmbedFromStory;
