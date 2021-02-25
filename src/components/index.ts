3c4f32c65a99d263dad1800a04e480de3ea008dc
Vinnie Olsen <vinnie.olsen@gmail.com>
2021-02-25 17:29:27 -0600

    Built LazyLoad component & unit tests

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,6 +4,7 @@ export { default as Gallery } from './Gallery';
 export { default as Image } from './Image';
 export { default as Video } from './Video';
 export { default as ImageMetadata } from './ImageMetadata';
+export { default as LazyLoad } from './LazyLoad';
 export { default as Lightbox } from './Lightbox';
 export { default as MetaData } from './MetaData';
 export { default as EventEmitter } from './EventEmitter';
