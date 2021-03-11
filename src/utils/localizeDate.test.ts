607f99aa59dad3a9957eea333b489bd059554375
jackhowa <5950956+JackHowa@users.noreply.github.com>
2021-03-11 15:54:03 -0600

    docs: Add todo in tests for passing in languages

--- src/utils/localizeDate.test.ts
+++ src/utils/localizeDate.test.ts
@@ -8,26 +8,25 @@ it('returns us east expected output with at', () => {
       'en',
       'America/New_York',
     ),
-  ).toMatchInlineSnapshot(
-    '"January 01, 2000 at  8:00 pm EST"',
-  );
+  ).toMatchInlineSnapshot('"January 01, 2000 at  8:00 pm EST"');
 });
 
 it('returns time zone for unspecified locale but falls back to english language for month names if not matched', () => {
   expect(
     localizeDateHelper(
       '2000-01-02 01:00',
-      '%B %d, %Y at %l:%M %P %Z',
+      '%B %d, %Y %l:%M %P %Z',
       // hindi
       // to match language should be "जनवरी 02, 2000 at  6:30 पूर्वाह्न IST"
-      // Snapshot: "January 02, 2000 at  6:30 am IST"
-      // Received: "जनवरी 02, 2000 at  6:30 पूर्वाह्न IST"
+      // Snapshot: "January 02, 2000 6:30 am IST"
+      // Received: "जनवरी 02, 2000 6:30 पूर्वाह्न IST"
+      // todo: should be able to pass in any language and country locale
       'hi_IN',
       // india has half-hour timezone +05:30
       // india standard time
       'Asia/Kolkata',
     ),
   ).toMatchInlineSnapshot(
-    '"January 02, 2000 at  6:30 am IST"',
+    '"January 02, 2000  6:30 am IST"',
   );
 });
