93db847b847ff210363a7c3fe931de9e4e5892cd
jackhowa <5950956+JackHowa@users.noreply.github.com>
2021-03-12 16:08:21 -0600

    feat: Restore default utc behavior, allow mixmatch locale and timezone

--- src/utils/localizeDate.test.ts
+++ src/utils/localizeDate.test.ts
@@ -14,7 +14,7 @@ it('returns us east expected output with at', () => {
 // without require timezones
 // eslint-disable-next-line max-len
 // const tz = require('timezone')(require('timezone/en_US.js'), require('timezone/sv_SE.js'), require('timezone/fr_FR.js'), require('timezone/nb_NO.js'), require('timezone/de_DE'), require('timezone/es_ES'), require('timezone/ja_JP'), require('timezone/ko_KR'));
-it('returns American english language and utc date for timezone not found est', () => {
+it('returns American english language and utc date for timezone not found', () => {
   expect(
     localizeDateHelper(
       '2000-01-02 01:00',
@@ -24,7 +24,9 @@ it('returns American english language and utc date for timezone not found est', 
       // india standard time
       'Asia/Kolkata',
     ),
-  ).toMatchInlineSnapshot('"January 01, 2000  8:00 pm EST"');
+  ).toMatchInlineSnapshot(
+    '"January 02, 2000  1:00 am UTC"',
+  );
 });
 
 it('returns seoul for locale ko', () => {
@@ -35,5 +37,5 @@ it('returns seoul for locale ko', () => {
       'ko',
       'Asia/Seoul',
     ),
-  ).toMatchInlineSnapshot('"January 02, 2000 10:00 am KST"');
+  ).toMatchInlineSnapshot('"1월 02, 2000 10:00 오전 KST"');
 });
--- src/utils/localizeDateHelper.ts
+++ src/utils/localizeDateHelper.ts
@@ -1,6 +1,26 @@
 /* eslint-disable @typescript-eslint/no-var-requires,global-require */
 // tz docs https://bigeasy.github.io/timezone/
-import tz from 'timezone';
+const tz = require('timezone')(
+  // supported timezones
+  // timezone is the quirks of the area time-wise (eg daylight savings) and timezone
+  require('timezone/Asia/Seoul'),
+  require('timezone/Asia/Tokyo'),
+  require('timezone/Europe/Madrid'),
+  require('timezone/Europe/Busingen'),
+  require('timezone/Europe/Paris'),
+  require('timezone/Europe/Stockholm'),
+  require('timezone/America/New_York'),
+
+  // supported locales
+  // locale is related to language and place (eg, Spanish and Mexico)
+  require('timezone/ko_KR'),
+  require('timezone/ja_JP'),
+  require('timezone/es_ES'),
+  require('timezone/sv_SE'),
+  require('timezone/en_US'),
+  require('timezone/de_DE'),
+  require('timezone/fr_FR'),
+);
 
 function localizeDateHelper(
   date: string,
@@ -37,53 +57,10 @@ function localizeDateHelper(
   }
 
   // Convert to UTC date
+  // utc time is not region-specific
   const utc = tz(date);
 
-  // performance improvement via https://github.com/bigeasy/timezone/issues/292#issuecomment-354597725
-
-  // 1. match locale.
-  // locale is related to language and place (eg, Spanish and Mexico)
-  // 2. require target locale and timezone
-  // timezone is the quirks of the area time-wise (eg daylight savings)
-  // 3. use the function along with utc time and target date format
-  // utc time is not region-specific
-  // targetDateFormat is like "%d %m" for day month, formatting
-
-  if (locale === 'ko_KR') {
-    const SEOUL = tz(require('timezone/Asia/Seoul'), require('timezone/ko_KR'));
-    return SEOUL(utc, 'Asia/Seoul', targetDateFormat);
-  }
-
-  if (locale === 'ja_JP') {
-    const TOKYO = tz(require('timezone/Asia/Tokyo'), require('timezone/ja_JP'));
-    return TOKYO(utc, 'Asia/Tokyo', targetDateFormat);
-  }
-
-  if (locale === 'es_ES') {
-    const MADRID = tz(require('timezone/Europe/Madrid'), require('timezone/es_ES'));
-    return MADRID(utc, locale, 'Europe/Madrid', targetDateFormat);
-  }
-
-  if (locale === 'de_DE') {
-    const BUSINGEN = tz(require('timezone/Europe/Busingen'), require('timezone/de_DE'));
-    return BUSINGEN(utc, locale, 'Europe/Busingen', targetDateFormat);
-  }
-
-  if (locale === 'fr_FR') {
-    const PARIS = tz(require('timezone/Europe/Paris'), require('timezone/fr_FR'));
-    return PARIS(utc, locale, 'Europe/Paris', targetDateFormat);
-  }
-
-  if (locale === 'sv_SE') {
-    const STOCKHOLM = tz(require('timezone/Europe/Stockholm'), require('timezone/sv_SE'));
-    return STOCKHOLM(utc, locale, 'timezone/Stockholm', targetDateFormat);
-  }
-
-  // default case
-  // if (locale === 'en_US') {
-  const NYC = tz(require('timezone/America/New_York'), require('timezone/en_US'));
-  return NYC(utc, locale, 'America/New_York', targetDateFormat);
-  // }
+  return tz(utc, locale, timeZone, targetDateFormat);
 }
 
 export default localizeDateHelper;
