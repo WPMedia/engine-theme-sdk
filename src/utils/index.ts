560074297ac678ec9a825eef7e1da97fb9c90466
Nick Schubach <nicholas.schubach@washpost.com>
2021-04-19 10:09:24 -0400

    Consolidate social media links generator to shared sdk utility

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,3 +2,4 @@ export { default as localizeDate } from './localizeDate';
 export { default as localizeDateTime } from './localizeDateTime';
 export { default as extractVideoEmbedFromStory } from './extractVideoEmbedFromStory';
 export { default as isServerSide } from './serverSide';
+export { default as constructSocialURL } from './constructSocialURL';
