616696e61418e0745513f1a4883c8ed7bfb29e35
Nick Schubach <nicholas.schubach@washpost.com>
2021-04-19 14:30:20 -0400

    Updated the documentation and enumerated the values for supported social site URLs

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,4 +2,4 @@ export { default as localizeDate } from './localizeDate';
 export { default as localizeDateTime } from './localizeDateTime';
 export { default as extractVideoEmbedFromStory } from './extractVideoEmbedFromStory';
 export { default as isServerSide } from './serverSide';
-export { default as constructSocialURL } from './constructSocialURL';
+export { default as constructSocialURL, SocialTypes as socialTypes } from './constructSocialURL';
