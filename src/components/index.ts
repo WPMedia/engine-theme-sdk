794c1e60fbf45b39d600741d46bb6ec9ddf3f254
Nick Schubach <nschubach@gmail.com>
2021-08-31 15:31:23 -0400

    TMEDIA-469: Add Error Icon

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,13 +1,13 @@
 export { default as ArcLogo } from './ArcLogo';
 export { default as ErrorBoundary } from './ErrorBoundary';
+export { default as EventEmitter } from './EventEmitter';
 export { default as Gallery } from './Gallery';
 export { default as Image } from './Image';
-export { default as Video } from './Video';
 export { default as ImageMetadata } from './ImageMetadata';
 export { default as LazyLoad } from './LazyLoad';
 export { default as Lightbox } from './Lightbox';
 export { default as MetaData } from './MetaData';
-export { default as EventEmitter } from './EventEmitter';
+export { default as Video } from './Video';
 export {
   default as VideoPlayer,
   videoPlayerCustomFields,
