65c8f8c3ccafa9e0d17af8aa6dbb1d384a943736
Jack Howard <jackhowa@fastmail.com>
2022-02-14 15:54:29 -0600

    fix: Ensure correct classname and structure is targeted for setting properties on video

--- src/components/VideoPlayer/formatEmbedMarkup.test.tsx
+++ src/components/VideoPlayer/formatEmbedMarkup.test.tsx
@@ -26,13 +26,6 @@ describe('If the embed html is valid', () => {
       + ' data-uuid="e924e51b" data-aspect-ratio="0.562" data-api="prod"><script '
       + 'src="//xxx.cloudfront.net/prod/powaBoot.js?org=corecomponents"></script></div>';
 
-    // in original test, the script tag was commented out strangely
-    // const expectedEmbed =
-    //   '<div class="powa"  data-autoplay=true data-muted=true' +
-    //   ' id="powa-e924" data-org="corecomponents" data-env="prod" data-uuid="e924e51b" ' +
-    //   'data-aspect-ratio="0.562" data-api="prod"><script src="//xxx.cloud' +
-    //   'front.net/prod/powaBoot.js?org=corecomponents"></script></div>';
-
     const embedHTMLOutput = formatEmbedMarkup(testEmbed, true, false);
     expect(embedHTMLOutput).toMatch(/data-autoplay="true"/i);
     expect(embedHTMLOutput).toMatchInlineSnapshot(
@@ -43,12 +36,6 @@ describe('If the embed html is valid', () => {
     const testEmbed = '<div class="powa" id="powa-e924" data-org="corecomponents" data-env="prod"'
       + ' data-uuid="e924e51b" data-aspect-ratio="0.562" data-api="prod"><script '
       + 'src="//d2w3jw6424abwq.cloudfront.net/prod/powaBoot.js?org=corecomponents"></script></div>';
-
-    // const expectedEmbed =
-    //   '<div class="powa"  data-autoplay=true data-muted=true  data-playthrough=true' +
-    //   ' id="powa-e924" data-org="corecomponents" data-env="prod" data-uuid="e924e51b" ' +
-    //   'data-aspect-ratio="0.562" data-api="prod"><script src="//d2w3jw6424abwq.cloud' +
-    //   'front.net/prod/powaBoot.js?org=corecomponents"></script></div>';
 
     const embedHTMLOutput = formatEmbedMarkup(testEmbed, true, true);
     expect(embedHTMLOutput).toMatch(/data-autoplay="true"/i);
@@ -74,8 +61,8 @@ describe('If the embed html is valid', () => {
   });
   it('passes in an override aspect ratio', () => {
     const testEmbed = '<div class="powa" id="powa-e924" data-org="corecomponents" data-env="prod"'
-    + ' data-uuid="e924e51b" data-aspect-ratio="0.562" data-api="prod"><script '
-    + 'src="//d2w3jw6424abwq.cloudfront.net/prod/powaBoot.js?org=corecomponents"></script></div>';
+      + ' data-uuid="e924e51b" data-aspect-ratio="0.562" data-api="prod"><script '
+      + 'src="//d2w3jw6424abwq.cloudfront.net/prod/powaBoot.js?org=corecomponents"></script></div>';
 
     // 0.3 target aspect ratio
     const embedHTMLOutput = formatEmbedMarkup(testEmbed, false, false, 0.3);
@@ -83,5 +70,27 @@ describe('If the embed html is valid', () => {
     expect(embedHTMLOutput).toMatch(/data-aspect-ratio="0.3"/i);
     // 0.562 was passed in default
     expect(embedHTMLOutput).not.toMatch(/data-aspect-ratio="0.562"/i);
+  });
+  it('handles a non-powa parent div correctly', () => {
+    const testEmbed = `
+      <div class="goldfish-player">
+        <div class="powa" id="powa-ff" data-org="ffff" data-uuid="ffff" data-aspect-ratio="0.562">
+          <script src="//ff.cloudfront.net/prod/powaBoot.js"></script>
+        </div>
+      </div>
+    `;
+    const embedHTMLOutput = formatEmbedMarkup(testEmbed, true, true);
+
+    // ensure no parent autoplay is added
+    expect(embedHTMLOutput.match(/<div class="goldfish-player">/i)[0]).toBe('<div class="goldfish-player">');
+
+    expect(embedHTMLOutput).toMatchInlineSnapshot(`
+      "<div class=\\"goldfish-player\\">
+              <div class=\\"powa\\" id=\\"powa-ff\\" data-org=\\"ffff\\" data-uuid=\\"ffff\\" data-aspect-ratio=\\"0.562\\" data-autoplay=\\"true\\" data-muted=\\"true\\" data-playthrough=\\"true\\">
+                <script src=\\"//ff.cloudfront.net/prod/powaBoot.js\\"></script>
+              </div>
+            </div>
+          "
+    `);
   });
 });
