c46d556662e92d4d1661cdff199f62fff86e2196
Jack Howard <jack.howard@washpost.com>
2022-05-27 20:25:52 +0000

    perf: Remove format url from engine theme sdk as it's already in themes components

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,4 +2,3 @@ export { default as localizeDate } from "./localizeDate";
 export { default as localizeDateTime } from "./localizeDateTime";
 export { default as extractVideoEmbedFromStory } from "./extractVideoEmbedFromStory";
 export { default as constructSocialURL } from "./constructSocialURL";
-export { default as formatURL } from "./formatURL";
