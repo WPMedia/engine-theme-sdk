93f3b7fbbaa12dda5b19f62c2a49074404cc08d0
Jack Howard <jackhowa@fastmail.com>
2022-08-03 16:28:16 -0500

    refactor: Remove unused icons. Use the arc themes components instead

--- src/components/icons/types.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable semi */
-export default interface IconProps {
-	width?: number | string;
-	height?: number | string;
-	fill?: string;
-	title?: string;
-	description?: string;
-	context?: "presentational" | "image";
-}
